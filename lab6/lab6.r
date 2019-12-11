c6748a141593f98aa80a199aab72afadad2e026a
KOCTYN <55509699+KOCTYN@users.noreply.github.com>
2019-12-11 22:57:44 +0300

    Update lab6.r

--- lab6/lab6.r
+++ lab6/lab6.r
@@ -1,6 +1,6 @@
 library("MASS")
 
-estimateMu <- function(x)
+vostanovleniemu <- function(x)
 {
   m <- dim(x)[2]
   mu <- matrix(NA, 1, m)
@@ -11,7 +11,7 @@ estimateMu <- function(x)
   return(mu)
 }
 
-estimateSigma <- function(x,mu)
+vostanovlenieсovariance <- function(x,mu)
 {
   n <- dim(x)[1]
   m <- dim(x)[2]
@@ -25,13 +25,39 @@ estimateSigma <- function(x,mu)
   return(sigma/(n-1))
 }
 
-naiv <- function(x,mu,sigma,P)
+naiv <- function(x,m,s,lamda,PP)
 {
-  p <- log(1,P)
-  p <- 0
-  sigma <- as.numeric(sigma)
-  pyj <- (1/(sqrt(2*pi*sigma^2)))*exp(-((x-mu)^2)/(2*sigma^2))
-  p <- p+log(pyj[1,1])+log(pyj[1,2])
+  
+  
+  n<-dim(mu)[2]
+  p<-rep(0,n)
+  a<-matrix(c(0,0,0,0),2,2)
+  for(i in 1:n)
+  {
+
+    sigma<-s[i]
+    mu<-matrix(c(m[i,1],m[i,2]),1,2)
+    l<-lamda[i]
+    P<-PP[i]
+    
+    pyj <- (1/(sqrt(2*pi*sigma^2)))*exp(-((x-mu)^2)/(2*sigma^2))
+    p[i] <- log(lamda*P)+log(pyj[1,1])+log(pyj[1,2])
+    
+  }
+  
+  
+  if(p[1]>p[2])
+  {
+    
+    class<-colors[1]
+  }
+  else
+  {
+    class<-colors[2]
+  }
+  return(class)
+  
+
   
   return(p)
 }
@@ -56,11 +82,22 @@ colors <- c("tan1", "royalblue")
 points(xy1, pch=21, col=colors[1], bg=colors[1])
 points(xy2, pch=21, col=colors[2], bg=colors[2])
 
-mu1 <- estimateMu(xy1)
-mu2 <- estimateMu(xy2)
+mu1 <- vostanovleniemu(xy1)
+mu2 <- vostanovleniemu(xy2)
 
-sigma1 <-estimateSigma(xy1,mu1)
-sigma2 <-estimateSigma(xy2,mu2)
+sigma1 <-vostanovlenieсovariance(xy1,mu1)
+sigma2 <-vostanovlenieсovariance(xy2,mu2)
+
+l1<-20
+l2<-6
+
+P1<-dim(xy1)[1]/(dim(xy1)[1]+dim(xy2)[1])
+P2<-dim(xy2)[1]/(dim(xy1)[1]+dim(xy2)[1])
+
+sigma<-rbind(sigma1,sigma2)
+mu<-rbind(mu1,mu2)
+l<-rbind(l1,l2)
+P<-rbind(P1,P2)
 
 x <- -10
 while(x < 10) 
@@ -69,15 +106,10 @@ while(x < 10)
   while (y < 10) 
   {
     z <- c(x,y)  
-    p1 <- naiv(z,mu1,sigma1,0.5)
-    p2 <- naiv(z,mu2,sigma2,0.5)
-    if(p1 > p2)  
-    {
-      class <- 1
-    }else{
-      class <- 2
-    }
-    points(z[1], z[2], pch = 21, col = colors[class])
+    
+    c <- naiv(z,mu,sigma,l,P)
+    points(z[1], z[2], pch = 21, col = c)
+
     y <- y+0.3
   }
   x <- x+0.3
